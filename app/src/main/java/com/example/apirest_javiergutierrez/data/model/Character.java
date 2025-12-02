123308818d241eeba5473fefd222b9acb3693153
lmfaogotdeleted <jgutierreza16@educarex.es>
2025-12-02 21:30:59 +0100

    Initial commit

---
+++ app/src/main/java/com/example/apirest_javiergutierrez/data/model/Character.java
@@ -0,0 +1,8 @@
+package com.example.apirest_javiergutierrez.data.model;
+
+public class Character {
+    private int id;
+    private String name;
+    private String status;
+    private String species;
+}
